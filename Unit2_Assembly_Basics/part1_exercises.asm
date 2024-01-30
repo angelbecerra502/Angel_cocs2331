9130a0b57fda77ff2356b3dd9c9df0e8d9c3e850
Angel Becerra <Angelbecerra502@gmail.com>
2024-01-30 16:43:34 +0000

    assembly part1

--- Unit2_Assembly_Basics/part1_exercises.asm
+++ Unit2_Assembly_Basics/part1_exercises.asm
@@ -19,10 +19,49 @@ segment .bss
 
 segment .text
         global  asm_main
-asm_main:
+asm_main: 
         enter   0,0               ; setup routine
         pusha
+        dump_regs 1
+        call print_nl
+        
+        mov eax,0; making eax = 0
+        mov ah,18; making ah = 18 (base 10)
+        mov al,0BAH; AL = 0xBA(base 16)
+        dump_regs 2
 
+        mov eax,0BEEFCAFEH
+        dump_regs 3
+
+        mov eax,3; eax += ebx, eax = 7
+
+        
+        mov ebx,4 ; ebx = 4 
+
+
+        add eax,ebx
+
+        mov eax,10
+        mov ebx,4
+
+        sub eax,ebx ; changes the value to the new value
+        inc ebx ;adds by inciment by 5
+
+        mov eax,ebx
+        inc ecx
+        dump_regs 4
+        call print_nl
+        
+        dump_regs 1
+
+        mov eax,4
+        add eax,eax
+        add eax,eax
+        add eax,eax
+
+        dump_regs 5
+
+        dump_regs 1; dump register
 		; TODO: add code
 
         popa
